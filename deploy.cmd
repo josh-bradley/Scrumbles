4da92ef226167a076fb8fa3623cb3dfbc0be94c4
joshjb84@hotmail.com <joshjb84@hotmail.com>
2015-03-08 10:25:31 +1100

    fix config

--- deploy.cmd
+++ deploy.cmd
@@ -108,7 +108,7 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
 ::  4. Install bower packages
 IF EXIST "%DEPLOYMENT_TARGET%\public\bower.json" (
      pushd "%DEPLOYMENT_TARGET%\public"
-     call :ExecuteCmd !NPM_CMD! install bower
+     call :ExecuteCmd !NPM_CMD! install "bower"
      IF !ERRORLEVEL! NEQ 0 goto error
      ./node_modules/.bin/bower install
         IF !ERRORLEVEL! NEQ 0 goto error
