2e42ed94e133a074cd0e516a0b1a7babd22174a1
joshjb84@hotmail.com <joshjb84@hotmail.com>
2017-03-09 12:27:19 +1100

    more log change

--- server/server.js
+++ server/server.js
@@ -12,8 +12,8 @@ function start(publicDir){
     var port = process.env.port || 3001;
 
     server.listen(port, function () {
-        console.log('the env.PORT was ' + process.env.port)
-        console.log('Server listening at port ' + server.address().port);
+        console.log('the env.PORT was can you believe ' + port)
+        console.log('Server listening at port ' + port);
     });
 
     // Routing
